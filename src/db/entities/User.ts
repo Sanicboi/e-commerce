c7e8ffea5f5ec56f488c25914359e4eabc727336
sanicboi <alexandrzezekalo@gmail.com>
2024-10-25 08:20:12 +0300

    Basic User Model

---
+++ src/db/entities/User.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    address: string;
+
+    @Column('date')
+    dateOfBirth: Date;
+}
